2184e27a61ae42c9bf54e65907f338aee0f22f51
thi anh thu <thu.hoang@onemount.com>
2021-08-19 17:08:42 +0700

    update exercise 4(a,b,c)

--- src/main/java/matrix.java
+++ src/main/java/matrix.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 
 public class matrix {
     public static void main(String[] args) {
-        int[][] a = generateMatrix(5, 7, 0, 100);
+        int[][] a = generateMatrix(4, 4, 0, 5);
         printMatrix(a);
         System.out.format("SUM = %d", sumMatric(a));
         System.out.println();
 
-//        if (isContainX(a, 3)) {
-//            System.out.println("Ma trận có chứa x");
-//        } else System.out.println("Ma trận không chứa x");
+        if (isContainX(a, 3)) {
+            System.out.println("Ma trận có chứa x");
+        } else System.out.println("Ma trận không chứa x");
 
         if (isPrimeMatrix(a)) {
             System.out.println("Ma trận toàn số nguyên tố");
         } else System.out.println("Không phải ma trận toàn số nguyên tố");
 
-//        int maxValue=maxValueMatrix(a);
-//        if(maxValue>0){
-//            System.out.println("Gía trị lớn nhất trong mảng " + maxValue);
-//        }
+
+        System.out.println("Tổng đường chéo chính" + sumDuongCheoChinh(a));
+        System.out.println("tổng đường chéo phụ" + sumDuongCheoPhu(a));
+        System.out.println(" tổng các phần tử trên dòng " + sumPhanTuDong(a, 2));
+        System.out.println(" tổng các phần tử trên cột " + sumPhanTuCot(a, 2));
+        System.out.println("tổng các phần tử trên đường chéo chính " + sumPhanTuNuaTrenDuongCheoChinh(a));
+        System.out.println("tổng các phần tử trên đường chéo chính " + sumPhanTuNuaDuoiDuongCheoChinh(a));
+
+        int maxValue=maxValueMatrix(a);
+        if(maxValue>0){
+            System.out.println("Gía trị lớn nhất trong mảng " + maxValue);
+        }
     }
 
     public static int[][] inputMatrix() {
@@ -84,26 +92,115 @@ public class matrix {
 
     }
 
+    public static boolean isPrimeNumber(int n) {
+        boolean ret = true;
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+
     public static boolean isPrimeMatrix(int[][] a) {
         for (int i = 2, r = a.length; i < r; i++) {
             for (int j = 2, c = a[0].length; j < c; j++) {
-                if (a[i][j] % i == 0) {
-                    return true;
+                if (!isPrimeNumber(a[i][j])) {
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     public static int maxValueMatrix(int[][] a) {
         int req = a[0][0];
         for (int i = 2, r = a.length; i < r; i++) {
             for (int j = 2, c = a[0].length; j < c; j++) {
-                if (a[i][j] > req ) {
-                    req =a[i][j];
+                if (a[i][j] > req) {
+                    req = a[i][j];
                 }
             }
         }
         return req;
+    }
+
+    public static int sumDuongCheoChinh(int[][] a) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (i == j) {
+                    sum += a[i][j];
+
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static int sumDuongCheoPhu(int[][] a) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (i + j == r - 1)
+                    sum += a[i][j];
+
+            }
+
+        }
+        return sum;
+    }
+
+    public static int sumPhanTuDong(int[][] a, int x) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (i == x) {
+                    sum += a[x][j];
+                }
+            }
+
+        }
+        return sum;
+    }
+
+    public static int sumPhanTuCot(int[][] a, int x) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (j == x) {
+                    sum += a[i][x];
+                }
+            }
+
+        }
+        return sum;
+    }
+
+    public static int sumPhanTuNuaTrenDuongCheoChinh(int[][] a) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (i < j) {
+                    sum += a[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static int sumPhanTuNuaDuoiDuongCheoChinh(int[][] a) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (i > j) {
+                    sum += a[i][j];
+                }
+            }
+
+
+        }
+        return sum;
     }
 }
